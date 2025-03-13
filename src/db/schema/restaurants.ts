3c7a50d8277062675e418a764e6522d36aa1a310
jr <junior_silvapereira@hotmail.com>
2025-03-12 21:21:42 -0300

    first commit

---
+++ src/db/schema/restaurants.ts
@@ -0,0 +1,15 @@
+import { createId } from '@paralleldrive/cuid2'
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core'
+
+
+export const restaurants = pgTable("restaurants", {
+  id: text('id')
+    .$defaultFn(() => createId())
+    .primaryKey(),
+  name: text('name').notNull(),
+  description: text('description'),
+  phone: text('phone'),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
