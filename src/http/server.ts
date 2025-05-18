26b2de2908cc18bb87f0726d8b26aed45119a25a
jr <junior_silvapereira@hotmail.com>
2025-05-18 14:08:02 -0300

    feat: auth-links route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,8 @@
 import { Elysia } from 'elysia'
-import { env } from '../env'
+import { registerRestaurant } from './routes/register-restaurant'
+import { sendAuthLink } from './routes/send-auth-link'
 
-const app = new Elysia().get('/', () => {
-  return `🔥 ~ PROCESS.ENV: ${JSON.stringify(env.DATABASE_URL)}`
-})
+const app = new Elysia().use(registerRestaurant).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
